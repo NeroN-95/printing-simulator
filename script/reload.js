da1ecf194cd7f45db784f0fca23bb5c9ddbbf3e1
unknown <romaburlaka1995@gmail.com>
2022-08-23 03:24:10 +0300

    adaptive layout for tablet and mobile

--- script/reload.js
+++ script/reload.js
@@ -16,14 +16,24 @@ document.getElementById('reset').onclick = function() {
     
     location.reload(); 
   }
+document.getElementById('reset_mob').onclick = function(){
+    location.reload();
+}
 
 
 
   let i=0;
   let image=document.getElementById("image");
-  var imgs=new Array('img/logo.png','img/logo2.png');
+  let imgs=new Array('img/logo.png','img/logo2.png');
   function imgsrc() {
       i++;i%=imgs.length;
       image.src=imgs[i];
   }
-   
+  let n=0;
+  let smallImage=document.getElementById("image_small");
+  let img=new Array('img/small_logo.png','img/small_logo2.png');
+  function imgSmallsrc() {
+      n++;n%=img.length;
+      smallImage.src=img[n];
+  }
+  
